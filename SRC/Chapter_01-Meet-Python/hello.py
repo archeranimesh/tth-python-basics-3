fc83fe6cf83907d6412dff0d1e268c96f1e73bb0
Animesh Bhadra <animeshb@archerimagine.com>
2020-10-28 23:20:51 +0530

    👌 IMPROVE: variables and multiple param to print.

--- SRC/Chapter_01-Meet-Python/hello.py
+++ SRC/Chapter_01-Meet-Python/hello.py
@@ -1,3 +1,8 @@
 print("Hello, World")
 print("Hello, Animesh")
 print("Animesh is learning Python")
+# Variables
+first_name = "Ada"
+
+# print takes multiple arguments.
+print("These", "will be", "joined together by spaces")
