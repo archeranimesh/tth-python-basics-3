4b221bf4bffdab6ce03706ac3f56be6331215bfd
Animesh Bhadra <animeshb@archerimagine.com>
2020-10-28 23:37:19 +0530

    📦 NEW: input statement.

--- SRC/Chapter_01-Meet-Python/hello.py
+++ SRC/Chapter_01-Meet-Python/hello.py
@@ -7,3 +7,7 @@ print(first_name, "is learning Python")
 
 # print takes multiple arguments.
 print("These", "will be", "joined together by spaces")
+
+# input statement.
+first_name = input("What is your first name? ")
+print("Hello,", first_name)
