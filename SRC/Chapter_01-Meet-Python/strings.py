80cf3a61eeee890eef4f0a5a693c3c1105884124
Animesh Bhadra <animeshb@archerimagine.com>
2020-11-01 00:05:21 +0530

    📦 NEW: String methods.

---
+++ SRC/Chapter_01-Meet-Python/strings.py
@@ -0,0 +1,13 @@
+quote = "A person who never made a mistake never tried anything new"
+print("Length of quote is: ", len(quote))
+print("UPPERCASE quote: ", quote.upper())
+print("Title Case of quote: ", quote.title())
+
+# String formatting.
+subject_template = "Thanks for learning {} with us {}."
+print(subject_template.format("Python", "Animesh"))
+
+buying_template = "You just bought {} {}."
+print(buying_template.format(3, "fidget cubes"))
+
+print("Hello" in "Hello, World")
