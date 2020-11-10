cd8d378ee6a46da6d91ac522cfb09071b1887a42
Animesh Bhadra <animeshb@archerimagine.com>
2020-11-10 23:26:34 +0530

    👌 IMPROVE: added error handling

--- SRC/Chapter_04-All-Together-Now/01_masterticket.py
+++ SRC/Chapter_04-All-Together-Now/01_masterticket.py
@@ -10,24 +10,31 @@ while tickets_remaining:
     name = input("What is your name?  ")
 
     # Prompt the user by name and ask how many tickets they would like
-    num_tickets = int(input("How many tickets would you like, {}? ".format(name)))
-
-    # Calculate the price (no of ticktes multiplied by the price) and assign it to a variable.
-    amount_due = num_tickets * TICKET_PRICE
-    # O/P the price to the screen.
-    print("The total due is ${}".format(amount_due))
-
-    # Prompt the user if they want to proceed. Y/N
-    should_proceed = input("Do you want to proceed with the purchase Y/N ? ")
-    # if they want to proceed
-    if should_proceed.lower() == "y":
-        # print out the screen "SOLD!" to confirm purchase
-        print("SOLD!")
-        # Decrement the tickets, remaining by the no of tickets purchased.
-        tickets_remaining -= num_tickets
-    # Otherwise...
+    try:
+        num_tickets = int(input("How many tickets would you like, {}? ".format(name)))
+        # Raise a ValueError if the request is for more tickets than are available.
+        if num_tickets > tickets_remaining:
+            raise ValueError(
+                "There are only {} tickets remaining".format(tickets_remaining)
+            )
+    except ValueError as ex:
+        print("We ran into an issue, {}. please try again.".format(ex))
     else:
-        # TPersonalized thank you.
-        print("Thanks {} for visiting.".format(name))
+        # Calculate the price (no of ticktes multiplied by the price) and assign it to a variable.
+        amount_due = num_tickets * TICKET_PRICE
+        # O/P the price to the screen.
+        print("The total due is ${}".format(amount_due))
+        # Prompt the user if they want to proceed. Y/N
+        should_proceed = input("Do you want to proceed with the purchase Y/N ? ")
+        # if they want to proceed
+        if should_proceed.lower() == "y":
+            # print out the screen "SOLD!" to confirm purchase
+            print("SOLD!")
+            # Decrement the tickets, remaining by the no of tickets purchased.
+            tickets_remaining -= num_tickets
+        # Otherwise...
+        else:
+            # TPersonalized thank you.
+            print("Thanks {} for visiting.".format(name))
 # Notify the user that the ticket is sold out.
 print("Sorry the ticket is sold")
